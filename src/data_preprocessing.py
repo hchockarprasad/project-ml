12d45684fbfe2ddf7ce924f8896ce09ccb5bec95
hchockarprasad <hchockarprasad@gmail.com>
2017-09-27 21:59:20 +0530

    add data preprocessing model

---
+++ src/data_preprocessing.py
@@ -0,0 +1,34 @@
+from sklearn.preprocessing import (
+    Imputer,
+    LabelEncoder,
+    OneHotEncoder,
+    StandardScaler
+)
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3].values
+
+# Taking care of missing data
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+# Encoding categorical data
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+x = onehotencoder.fit_transform(x).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Feature scaling
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
